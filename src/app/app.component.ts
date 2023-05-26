858059d46e0a38fff84512403cc329799673a195
guillaume31100 <g.letalle@koncept-is.fr>
2023-05-26 10:09:52 +0200

    navigation effectué

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,36 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { POKEMONS } from './mok-pokemon-list';
-import { Pokemon } from './pokemon';
-
+import { Component} from '@angular/core';
 
 @Component({
   // selector nom du composant
   selector: 'app-root',
-
   // template = la vue
   templateUrl: 'app.component.html'
 })
 
-// export pour import dans app.module
-// implements OnInit pour méthode ngOnInit = au lancement de l'appli
-export class AppComponent implements OnInit{
-  pokemonList: Pokemon[] = POKEMONS;
-  pokemonSelected?: Pokemon|undefined;
-
-  // Méthode qui se lance a l'ouverture de l'appli
-  ngOnInit() {
-    console.table(this.pokemonList);
-  }
-
-  selectPokemon(pokemonId: string){
-     const pokemon:Pokemon|undefined = this.pokemonList.find(pokemon =>pokemon.id == +pokemonId);
-
-     if(pokemon){
-       console.log(`vous avez demandé le pokémon ${pokemon.name}`);
-       this.pokemonSelected = pokemon;
-     }else{
-      console.log(`vous avez demandé un pokemon qui n'existe pas`);
-      this.pokemonSelected = pokemon;
-     }
-  }
-}
+export class AppComponent {}
--- src/app/border-card.directive.ts
+++ src/app/border-card.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, ElementRef, HostListener } from '@angular/core';
+import { Directive, ElementRef, HostListener, Input } from '@angular/core';
 
 @Directive({
   //directive attribut => entouré []
@@ -6,30 +6,38 @@ import { Directive, ElementRef, HostListener } from '@angular/core';
 })
 export class BorderCardDirective {
 
+  private initialColor: string = '#f5f5f5';
+  private defaultColor: string = '#009688';
+  private defaultHeight: number = 200;
+
+
   //ElementRef => Composant de Angular/core qui permet de récuperer une reference du DOM
   constructor(private el: ElementRef) {
-    this.setHeight(200);
-    this.setBorder('#f5f5f5');
+    this.setHeight(this.defaultHeight);
+    this.setBorder(this.initialColor);
   }
+
+  //propriété d'entée pour la directive
+  @Input('pkmnBorderCard') bordercolor: string;//alias
 
   //Permet de lier une méthode de la directive à un evenement donné
   @HostListener('mouseenter') onMouseEnter() {
-    this.setBorder('#009688');
+    this.setBorder(this.bordercolor || this.defaultColor);
   }
-
   @HostListener('mouseleave') onMouseLeave() {
-    this.setBorder('#f5f5f5');
+    this.setBorder(this.initialColor);
   }
 
   // Méthode qui permet de définir la hauteur du elementRef
-  private setHeight(height: number) {
+  private setHeight(defaultHeight: number) {
     // this pour accédder a la ref de l'élémént 
     //nativeElement pour accéder à l'élément natif
-    this.el.nativeElement.style.height = `${height}px`
+    this.el.nativeElement.style.height = `${defaultHeight}px`
   }
 
-  private setBorder(color: string) {
-    this.el.nativeElement.style.border = `solid 4px ${color} `
+  // Méthode qui permet de définir la couleur du border initial du elementRef
+  private setBorder(initialColor: string) {
+    this.el.nativeElement.style.border = `solid 4px ${initialColor} `
   }
 
 }
